0d07e7495caa9bd5b6c98f693ba5bc0667569a56
kamaboko1 <github65@hotmail.com>
2019-05-20 13:26:08 -0700

    counter with one stretch goal

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,7 +10,7 @@ class Counter extends Component {
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
-        // increments after waiting for one second
+        setTimeout(this.props.increment, 1000)
     };
 
     render() {
@@ -20,20 +20,20 @@ class Counter extends Component {
         return (
             <p>
                 Clicked: {this.props.count} times
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {this.props.increment() }}>
                     +
                 </button>
-                <button onClick={() => {/* Fill me in */ }}>
+                <button onClick={() => {this.props.decrement()}}>
                     -
                 </button>
                  {/* Uncomment these button tags if you got
                 around to implementing the extra credit functions */}
                 {/* <button onClick={this.incrementIfOdd}>
                     Increment if odd
-                </button>
+                </button> */}
                 <button onClick={this.incrementAsync}>
                     Increment async
-                </button>  */}
+                </button> 
             </p>
         );
     }
@@ -47,9 +47,12 @@ class Counter extends Component {
 // parts it needs from the state object.
 const mapStateToProps = (state) => {
     return {
-        count: state.count
+        count: state
     };
 };
+
+// by changing the reducer to count 0, that messed up mapStateToProps so I had to just use state
+// rather than what I was expecting to use, state.count.  I probably messed this all up but it works.
 
 // The connect function is called in order to make this component aware
 // of the rest of the redux architecture. Without this, this component
